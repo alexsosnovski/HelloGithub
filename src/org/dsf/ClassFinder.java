5ab8eedbabb5557006c8162db7c65aa3530e3c24
Aleksei Sosnovski <alexsosnovski@gmail.com>
2013-02-02 01:00:06 +0200

    Project structure + one test that has not been tested. Hello from TDD.

---
+++ src/org/dsf/ClassFinder.java
@@ -0,0 +1,14 @@
+package org.dsf;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class ClassFinder {
+	public ClassFinder(InputStream classNamesStream) {
+	}
+
+	public Collection<String> findMatching(String pattern) {
+		return new LinkedList<>();
+	}
+}
---
+++ test/org/dsf/TestRunner.java
@@ -0,0 +1,41 @@
+package org.dsf;
+
+import java.io.InputStream;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class TestRunner {
+	private InputStream testStream;
+	private ClassFinder classFinder;
+	
+	@Test
+	private void testOneCapitalChar() {
+		Collection<String> result = classFinder.findMatching("T");
+		String[] arr = result.toArray(new String[result.size()]);
+		assertEquals(3, arr.length);
+		assertEquals("foo.bar.TestClass", arr[0]);
+		assertEquals("foo.bar.TestClassSecond", arr[1]);
+		assertEquals("foo.bar.TestClassThird", arr[2]);
+	}
+	
+	@Before
+	private void setUp() {
+		testStream = this.getClass().getClassLoader().getResourceAsStream("classNames1.txt");
+		classFinder = new ClassFinder(testStream);
+	}
+	
+	@After
+	private void tearDown() throws Exception {
+		if (testStream != null) {
+			testStream.close();
+		}
+	}
+}
