41514a8a2e497d4c0d3c282ac89ad58979ec17c2
Aleksei Sosnovski <alexsosnovski@gmail.com>
2013-02-02 01:13:00 +0200

    More tests.

--- test/org/dsf/TestRunner.java
+++ test/org/dsf/TestRunner.java
@@ -26,6 +26,33 @@ public class TestRunner {
 		assertEquals("foo.bar.TestClassThird", arr[2]);
 	}
 	
+	@Test
+	private void testTwoCapitalChars() {
+		Collection<String> result = classFinder.findMatching("TC");
+		String[] arr = result.toArray(new String[result.size()]);
+		assertEquals(3, arr.length);
+		assertEquals("foo.bar.TestClass", arr[0]);
+		assertEquals("foo.bar.TestClassSecond", arr[1]);
+		assertEquals("foo.bar.TestClassThird", arr[2]);
+	}
+	
+	@Test
+	private void testSpecifyingChars() {
+		Collection<String> result = classFinder.findMatching("TeCla");
+		String[] arr = result.toArray(new String[result.size()]);
+		assertEquals(3, arr.length);
+		assertEquals("foo.bar.TestClass", arr[0]);
+		assertEquals("foo.bar.TestClassSecond", arr[1]);
+		assertEquals("foo.bar.TestClassThird", arr[2]);
+	}
+	
+	@Test
+	private void testSpecifyingCharsNoResult() {
+		Collection<String> result = classFinder.findMatching("TeCzz");
+		String[] arr = result.toArray(new String[result.size()]);
+		assertEquals(0, arr.length);
+	}
+	
 	@Before
 	private void setUp() {
 		testStream = this.getClass().getClassLoader().getResourceAsStream("classNames1.txt");
